a1a630ae0ee1bcbdc8a132c6d54612605f38bf23
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-08-19 12:36:03 -0400

    feat: allows markov chains to be based on any number of previous words

--- generateMarkov.js
+++ generateMarkov.js
@@ -2,14 +2,14 @@ const fs = require(`fs`)
 
 const text = fs.readFileSync(`./cleanDanaherPosts.txt`, `utf8`)
 
-const generateFreqTable = () => {
+const generateFreqTable = (num) => {
   const arr = text.split(` `)
   const freqTable = {
     endsPlzNoCollisions : {},
     startsPlzNoCollisions : {},
   }
-  for(let i=1; i<arr.length; i++){
-    const prev = arr[i-1]
+  for(let i=num; i<arr.length; i++){
+    const prev = arr.slice(i-num, i).join(` `)
     const word = arr[i]
     if(word[word.length-1] === `.` || word[word.length-1] === `!` || word[word.length-1] === `?`){
       if(!freqTable.endsPlzNoCollisions[word]){
@@ -19,10 +19,11 @@ const generateFreqTable = () => {
       }
     }
     if(prev[prev.length-1] === `.` || prev[prev.length-1] === `!` || prev[prev.length-1] === `?`){
-      if(!freqTable.startsPlzNoCollisions[word]){
-        freqTable.startsPlzNoCollisions[word] = 1
+      const startWord = arr.slice(i, i+num).join(` `)
+      if(!freqTable.startsPlzNoCollisions[startWord]){
+        freqTable.startsPlzNoCollisions[startWord] = 1
       }else{
-        freqTable.startsPlzNoCollisions[word]++
+        freqTable.startsPlzNoCollisions[startWord]++
       }
     }
     if(!freqTable[prev]){
@@ -54,12 +55,10 @@ const freqToMarkov = freqTable => {
   return freqTable
 }
 
-const generateMarkov = () => {
-  const freqtable = generateFreqTable()
+const generateMarkov = (markovNum) => {
+  const freqtable = generateFreqTable(markovNum)
   return freqToMarkov(freqtable)
 }
-
-const markov = generateMarkov()
 
 const pickNextWord = prev => {
   const selector = Math.random()
@@ -70,14 +69,16 @@ const pickNextWord = prev => {
   return ``
 }
 
-const generateQuote = num => {
-  const res = [pickNextWord(markov.startsPlzNoCollisions)]
-  for(let i=1; i<num-1; i++){
-    res.push(pickNextWord(markov[res[i-1]]))
+const generateQuote = (length, markovNum) => {
+  const res = pickNextWord(markov.startsPlzNoCollisions).split(` `)
+  for(let i=markovNum; i<length-markovNum; i++){
+    res.push(...pickNextWord(markov[res.slice(-markovNum).join(` `)]).split(` `))
   }
   return res.join(` `)
 }
 
-const quote = generateQuote(200)
+const markov = generateMarkov(3)
+
+const quote = generateQuote(200, 3)
 
 console.log(quote)
