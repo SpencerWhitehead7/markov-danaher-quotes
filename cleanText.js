f0e08c112f31d063796821ea2238942c01269407
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-08-19 14:38:02 -0400

    refactor: cleans text more reliably

--- cleanText.js
+++ cleanText.js
@@ -2,21 +2,28 @@ const fs = require(`fs`)
 
 let text = fs.readFileSync(`./danaherPosts.txt`, `utf8`)
 
-text = text.replace(/[^\w\s.!?,\/;'-@]|[_]/gi, ``).replace(/[\r\n]+/g,` `).replace(/\s\s+/g, ` `)
+// strip special chars
+text = text.replace(/[^\w\s.!?',/@&\-']|_/gi, ``)
+
+// replace all sentence markers with marker plus space to split accidentally merged words
+text = text.replace(/\./g, `. `).replace(/\!/g, `! `).replace(/\?/g, `? `)
+
+// replace all newlines with spaces
+text = text.replace(/[\r\n]+/g,` `)
+
+// replace all multiple spaces with single spaces
+text = text.replace(/\s\s+/g, ` `)
 
 const arr = text.split(` `)
 
 const newArr = []
 
 arr.forEach(word => {
-  if((word.includes(`.`) && word[word.length-1] !== `.`) ||
-  (word.includes(`!`) && word[word.length-1] !== `!`) ||
-  (word.includes(`?`) && word[word.length-1] !== `?`)){
-    word.replace(/\./g, `. `)
-    word.replace(/\!/g, `! `)
-    word.replace(/\?/g, `? `)
-    newArr.push(word)
-  }else if(word.length > 1 || word === `I` || word === `a` || word === `A`){
+  if(word.length > 1 ||
+  word === `I` ||
+  word === `a` ||
+  word === `A` ||
+  word ===`&`){
     newArr.push(word)
   }
 })
