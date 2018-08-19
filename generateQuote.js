d149a3bbad425e7eb8e9e822e63c01247cd94f47
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-08-19 17:32:43 -0400

    edit: corrects length of quote

--- generateQuote.js
+++ generateQuote.js
@@ -13,7 +13,7 @@ const pickNextWord = prev => {
 
 const generateQuote = (length, markovNum) => {
   const res = pickNextWord(markov.startsPlzNoCollisions).split(` `)
-  for(let i=markovNum; i<length-markovNum; i++){
+  for(let i=markovNum; i<length; i++){
     res.push(...pickNextWord(markov[res.slice(-markovNum).join(` `)]).split(` `))
   }
   return res.join(` `)
@@ -22,3 +22,25 @@ const generateQuote = (length, markovNum) => {
 const quote = generateQuote(200, 3)
 
 console.log(quote)
+
+// // Even I don't even know what I'm thinking here
+// const endSentence = prev => {
+//   selector = Math.random()
+//   const nextWords = Object.keys(prev).filter(word => {
+//     return (word[word.length -1] === `.` ||
+//     word[word.length -1] === `!` ||
+//     word[word.length -1] === `?`)
+//   })
+//   if(nextWords.length === 0){
+//     pickNextWord(prev)
+//   }
+// }
+
+// const generateSentence = (markovNum, prev) => {
+//   const res = prev ? pickNextWord(markov[prev]).split(` `) : pickNextWord(markov.startsPlzNoCollisions).split(` `)
+//   const length = 25 + Math.round(Math.random() * (Math.random() > .5 ? 10 : -10))
+//   for(let i=markovNum; i<length; i+=markovNum){
+//     res.push(...pickNextWord(markov[res.slice(-markovNum).join(` `)]).split(` `))
+//   }
+//   return res.join(` `)
+// }
