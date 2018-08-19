39be85cc546ba135bd937c6560a52e08c55267f9
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-08-19 08:48:00 -0400

    resource: description of how to manually scrape FB for post texts

---
+++ scraping.js
@@ -0,0 +1,17 @@
+// on facebook page console:
+// setInterval(() => window.scrollTo(0,document.body.scrollHeight), 100)
+// which returns number
+// wait til you get to the bottom
+// clearInterval(that number)
+
+// get posts list
+// const allPosts = Array(...document.getElementsByClassName("text_exposed_root"))
+// expose fulltexts instead of truncated versions
+// allPosts.forEach(root => root.className += " text_exposed")
+
+// turn text into a string stored in variable
+// let allText = ``
+// allPosts.forEach(root => allText += root.childNodes[0].innerText)
+
+// allText
+// then copy result into .txt file
