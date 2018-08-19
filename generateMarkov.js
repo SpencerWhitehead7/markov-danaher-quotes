cc0065586b5909841c5a39fe2925924e389fb6b3
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-08-19 12:41:18 -0400

    refactor: splits markov and quote generation into separate files

--- generateMarkov.js
+++ generateMarkov.js
@@ -60,25 +60,6 @@ const generateMarkov = (markovNum) => {
   return freqToMarkov(freqtable)
 }
 
-const pickNextWord = prev => {
-  const selector = Math.random()
-  const nextWords = Object.keys(prev)
-  for(let i=0; i<nextWords.length; i++){
-    if(selector >= prev[nextWords[i]][0] && selector < prev[nextWords[i]][1]) return nextWords[i]
-  }
-  return ``
-}
-
-const generateQuote = (length, markovNum) => {
-  const res = pickNextWord(markov.startsPlzNoCollisions).split(` `)
-  for(let i=markovNum; i<length-markovNum; i++){
-    res.push(...pickNextWord(markov[res.slice(-markovNum).join(` `)]).split(` `))
-  }
-  return res.join(` `)
-}
-
 const markov = generateMarkov(3)
 
-const quote = generateQuote(200, 3)
-
-console.log(quote)
+module.exports = markov
---
+++ generateQuote.js
@@ -0,0 +1,22 @@
+const markov = require(`./generateMarkov`)
+
+const pickNextWord = prev => {
+  const selector = Math.random()
+  const nextWords = Object.keys(prev)
+  for(let i=0; i<nextWords.length; i++){
+    if(selector >= prev[nextWords[i]][0] && selector < prev[nextWords[i]][1]) return nextWords[i]
+  }
+  return ``
+}
+
+const generateQuote = (length, markovNum) => {
+  const res = pickNextWord(markov.startsPlzNoCollisions).split(` `)
+  for(let i=markovNum; i<length-markovNum; i++){
+    res.push(...pickNextWord(markov[res.slice(-markovNum).join(` `)]).split(` `))
+  }
+  return res.join(` `)
+}
+
+const quote = generateQuote(200, 3)
+
+console.log(quote)
