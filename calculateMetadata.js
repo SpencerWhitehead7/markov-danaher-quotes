4a241ec577d045942c5a6f594c7a0558dc130b61
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-08-19 16:53:16 -0400

    feat: calculate average words/sentence

--- calculateMetadata.js
+++ calculateMetadata.js
@@ -1,3 +1,25 @@
 const fs = require(`fs`)
 
 const text = fs.readFileSync(`cleanDanaherPosts.txt`, `utf8`)
+
+const mostSentences = text.split(`.`)
+
+const allSentences = []
+
+mostSentences.forEach(sentence => {
+  if(sentence.includes(`!`)){
+    allSentences.push(...sentence.split(`!`))
+  }else if(sentence.includes(`?`)){
+    allSentences.push(...sentence.split(`?`))
+  }else{
+    allSentences.push(sentence)
+  }
+})
+
+let totalWords = 0
+
+allSentences.forEach(sentence => {
+  totalWords += sentence.split(` `).length
+})
+
+console.log(totalWords/allSentences.length)
