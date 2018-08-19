4cdb6a1b30bece09b18e78549f9308d413190dbb
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-08-19 14:07:59 -0400

    refactor: cleans text more reliably

--- cleanText.js
+++ cleanText.js
@@ -2,15 +2,19 @@ const fs = require(`fs`)
 
 let text = fs.readFileSync(`./danaherPosts.txt`, `utf8`)
 
-text = text.replace(/[^\w\s.!?,;'-]|[_]/gi, ``).replace(/[\r\n]+/g,` `).replace(/\s\s+/g, ` `)
+text = text.replace(/[^\w\s.!?,\/;'-@]|[_]/gi, ``).replace(/[\r\n]+/g,` `).replace(/\s\s+/g, ` `)
 
 const arr = text.split(` `)
 
 const newArr = []
 
 arr.forEach(word => {
-  if(word.includes(`.`) && word[word.length-1] !== `.`){
+  if((word.includes(`.`) && word[word.length-1] !== `.`) ||
+  (word.includes(`!`) && word[word.length-1] !== `!`) ||
+  (word.includes(`?`) && word[word.length-1] !== `?`)){
     word.replace(/\./g, `. `)
+    word.replace(/\!/g, `! `)
+    word.replace(/\?/g, `? `)
     newArr.push(word)
   }else if(word.length > 1 || word === `I` || word === `a` || word === `A`){
     newArr.push(word)
