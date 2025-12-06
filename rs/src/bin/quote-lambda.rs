e96df2c2679ed92db60ec0e7b84dae3d3adf1bde
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2025-12-06 01:48:12 -0500

    refac: update quote-lambda, use std instead of lazy_static

--- rs/src/bin/quote-lambda.rs
+++ rs/src/bin/quote-lambda.rs
@@ -1,5 +1,6 @@
+use std::sync;
+
 use lambda_runtime;
-use lazy_static;
 use serde;
 
 use markov_danaher_quotes::MarkovChain;
@@ -14,15 +15,22 @@ struct Response {
   quote: String,
 }
 
-lazy_static::lazy_static! {
-  static ref MARKOV: MarkovChain =
-    ciborium::from_reader(include_bytes!("../../../rsResources/markov3") as &[u8]).unwrap();
+fn markov() -> &'static MarkovChain {
+  static MARKOV: sync::OnceLock<MarkovChain> = sync::OnceLock::new();
+  MARKOV.get_or_init(|| {
+    ciborium::from_reader(include_bytes!("../../../rsResources/markov3") as &[u8]).unwrap()
+  })
 }
 
 const DEFAULT_SENTENCE_COUNT: usize = 10;
 
 #[tokio::main]
 async fn main() -> Result<(), lambda_runtime::Error> {
+  // initialize the markov during cold-start, not first execution
+  // I don't think this ends up making any practical difference
+  // it's just a little more aesthetic
+  let _ = markov();
+
   let func = lambda_runtime::service_fn(func);
   lambda_runtime::run(func).await?;
   Ok(())
@@ -36,7 +44,8 @@ async fn func(
     .sentence_count
     .unwrap_or(DEFAULT_SENTENCE_COUNT)
     .clamp(1, 100);
-  let quote = MARKOV.generate_quote_by_sentences(sentence_count);
+
+  let quote = markov().generate_quote_by_sentences(sentence_count);
 
   Ok(Response { quote })
 }
